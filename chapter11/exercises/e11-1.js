09d2bc30e43cf2a5aff7e9442d1c4e4690660549
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-07-12 08:39:54 -0600

    E11-1 check if working

--- chapter11/exercises/e11-1.js
+++ chapter11/exercises/e11-1.js
@@ -1,6 +1,19 @@
 // 11.10.1. Practice Your Skills
 //Create an anonymous fucntion and set it equal to a variable.
 
+let input = function (n) {
+  if (typeof(n) == 'number') {
+    return n *3;
+  } else if (typeof(n) == 'string') {
+    return "ARRR!";
+  } else {
+    return n;
+  }
+}
+
+console.log (input(200));
+console.log (input('help'));
+console.log (input([1,2,3]));
 
 
 /* Your function should:
@@ -17,3 +30,7 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+console.log (arr.map(input));
+
+console.log(arr);
