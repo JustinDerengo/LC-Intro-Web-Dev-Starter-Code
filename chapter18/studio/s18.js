d216f27dd62a96b6ff435436ce63b7c9a16c1259
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-07-19 12:39:16 -0600

    18 studio

--- chapter18/studio/s18.js
+++ chapter18/studio/s18.js
@@ -1,7 +1,53 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
 
+class CrewCandidate {
+    constructor(name, mass, scores){
+        this.name = name;
+        this.mass = mass;
+        this.scores = scores;
+    }
+    addScore(newScore) {
+        this.scores.push(newScore);
+    }
+    average() {
+        let scoreSum = 0;
+        for (let i = 0; i < this.scores.length; i++) {
+            scoreSum += this.scores[i];
+        }
+        // let average = Math.round(scoreSum / this.scores.length) / 10
+        return Math.round(scoreSum / this.scores.length * 10) / 10
+    }
+    status(score) {
+        let acceptance = '';
+        if (score >= 90) {
+            acceptance = 'Accepted';
+        } else if (score >= 80) {
+            acceptance = 'Reserve';
+        } else if (score >= 70) {
+            acceptance = 'Probationary';
+        } else {
+            acceptance = 'Rejected';
+        }
+        
+        return `${this.name} has earned an average test score of ${score} and has a status of ${acceptance}`;
+    }
+}
+
+let bear = new CrewCandidate('Bubba Bear', 135, [88, 85, 90]);
+let dog = new CrewCandidate('Merry Maltese', 1.5, [93, 88, 97]);
+let alligator = new CrewCandidate('Glad Gator', 225, [75, 78, 62]);
+
+console.log(bear);
+console.log(dog);
+console.log(alligator);
+
+bear.addScore(99);
+console.log(bear.scores);
+console.log(dog.average());
+alligator.addScore(100);
 
 
+console.log(alligator.status(alligator.average()));
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 
 
--- extra_practice/libraryCatalogue.js
+++ extra_practice/libraryCatalogue.js
@@ -1,7 +1,111 @@
+const input = require('readline-sync');
+
 class Book {
     constructor(title, author, publicationYear) {
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
     }
+}
+
+let library = [];
+
+function addBookToLibrary () {
+        newTitle = input.question("Enter the book title: ");
+        newAuthor = input.question("Enter the author's name: ");
+        newYear = input.question("Enter the publication year: ");
+        newBook = newTitle;
+
+        newBook = new Book(newTitle, newAuthor, newYear);
+        library.push(newBook);
+
+        return console.log('Book added succesfully!');
+    }
+
+function displayBooks () {
+    for (let i = 0; i < library.length; i++) {
+        console.log(library[i]);
+    }
+}
+
+function serachBookByTitle(search) {
+    for (let i = 0; i < library.length; i++) {
+        if (library[i].title == search) {
+            console.log(library[i]);
+        }
+        console.log("No such book found in library.")
+        
+    }
+}
+
+function removeBookByTitle(search) {
+   for (let i = 0; i < library.length; i++) {
+        if(library[i].title = search) {
+            library.splice(library[i], 1);
+        }
+        console.log("No such book found in library.");
+    }
+}
+
+console.log("Welcome to the Library Catalog!");
+
+let choice = Number(input.question(`Menu:
+1. Add a book
+2. Display all books
+3. Search for a book
+4. Remove a book
+5. Exit
+
+Please enter your choice:`));
+
+while (choice !==5) {
+    if (choice < 1 || choice > 5){
+        choice = Number(input.question("Ivalid choice, please chose an option 1 -5: ")) 
+    } else if (choice === 1) {
+            addBookToLibrary();
+            choice = Number(input.question(`Menu:
+            1. Add a book
+            2. Display all books
+            3. Search for a book
+            4. Remove a book
+            5. Exit
+            
+            Please enter your choice:`));
+     } else if (choice === 2) {
+            displayBooks();
+            choice = Number(input.question(`Menu:
+            1. Add a book
+            2. Display all books
+            3. Search for a book
+            4. Remove a book
+            5. Exit
+            
+            Please enter your choice:`));
+    } else if (choice === 3) {
+            let search = input.question("Enter the title of the book you want to search: ");
+            serachBookByTitle(search);
+            choice = Number(input.question(`Menu:
+            1. Add a book
+            2. Display all books
+            3. Search for a book
+            4. Remove a book
+            5. Exit
+            
+            Please enter your choice:`));
+    } else if (choice === 4) {
+            let remove = input.question("Enter the title of the book you would like to remove: ");
+            removeBookByTitle(remove);
+            choice = Number(input.question(`Menu:
+            1. Add a book
+            2. Display all books
+            3. Search for a book
+            4. Remove a book
+            5. Exit
+            
+            Please enter your choice:`));
+    }
+}
+
+if (choice === 5) {
+    console.log("Thank you for using the LIbrary Catalog. Goodbye!");
 }
