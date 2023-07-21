61d05a22e2284e6d79de14924d343b0db035d0d8
Allegorylla <138933736+JustinDerengo@users.noreply.github.com>
2023-07-21 14:01:18 -0600

    created assignment folder

--- extra_practice/practice1.js
+++ extra_practice/practice1.js
@@ -1,3 +1,26 @@
 function greetUser(name) {
 return `Hello ${name}!`;
 }
+
+let student1 = {
+    name: 'John Smith',
+    gradeLevel: 4,
+
+    passGrade: function() {
+        this.gradeLevel++;
+    }
+};
+
+let student2 = {
+    name: 'Jane Smith',
+    gradeLevel: 9,
+
+    passGrade: function() {
+        this.gradeLevel++;
+    }
+};
+
+student1.passGrade();
+student2.passGrade();
+
+console.log(student1.gradeLevel);
